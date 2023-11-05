9f70099502c4cbf74521f71176743784492794f4
David Alejandro Medrano <102700809+Davidmedrano15@users.noreply.github.com>
2023-11-04 20:09:01 -0500

    done

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const { DB_HOST, PORT = 3000 } = process.env;
+// Cadena de conexión a tu base de datos MongoDB
+const dbURI = 'mongodb://localhost:27017/nombre-de-tu-base-de-datos';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión a la base de datos exitosa');
+    // Resto del código de tu aplicación
+  })
+  .catch((error) => console.error('Error de conexión a la base de datos:', error));
